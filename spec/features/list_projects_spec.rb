7c838e3b917e398f7335f06403dedc05940be315
mateusz <mateuszalmannai@googlemail.com>
2015-07-12 17:35:40 +0200

    spec out project details

--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 describe "Viewing the list of projects" do
   it "shows the projects stored in the database" do
 
-projectA = Project.create(name: "Start-Up Project",
-                            description: "A description of a start-up project",
-                            target_pledge_amount: 100.00,
-                            pledging_ends_on: 1.day.from_now,
-                            website: "http://project-a.com")
+    projectA = Project.create(name: "Start-Up Project",
+                              description: "A description of a start-up project",
+                              target_pledge_amount: 100.00,
+                              pledging_ends_on: 1.day.from_now,
+                              website: "http://project-a.com")
 
-  projectB = Project.create(name: "Community Project",
-                            description: "A description of a community project",
-                            target_pledge_amount: 200.00,
-                            pledging_ends_on: 1.week.from_now,
-                            website: "http://project-b.com")
+    projectB = Project.create(name: "Community Project",
+                              description: "A description of a community project",
+                              target_pledge_amount: 200.00,
+                              pledging_ends_on: 1.week.from_now,
+                              website: "http://project-b.com")
 
-  projectC = Project.create(name: "Personal Project",
-                            description: "A description of a person project",
-                            target_pledge_amount: 300.00,
-                            pledging_ends_on: 1.month.from_now,
-                            website: "http://project-c.com")
+    projectC = Project.create(name: "Personal Project",
+                              description: "A description of a person project",
+                              target_pledge_amount: 300.00,
+                              pledging_ends_on: 1.month.from_now,
+                              website: "http://project-c.com")
 
-  visit projects_url
+    visit projects_url
 
 
     expect(page).to have_text("3 Projects")
