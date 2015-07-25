5bdfb2ee958a662903d2c285dd1825dad9a26804
mateusz <mateuszalmannai@googlemail.com>
2015-07-25 21:24:10 +0200

    add model spec

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+  def format_pledging_ends_on(project)
+    if project.pledging_expired?
+      #content_tag(:strong, time_ago_in_words(project.pledging_ends_on) + ' ago.')
+      content_tag(:strong, 'All Done!')
+    else
+      distance_of_time_in_words(Date.today, project.pledging_ends_on) + " remaining"
+    end
+  end
 end
--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -7,7 +7,22 @@ describe "Viewing an individual project" do
     expect(page).to have_text(project.name)
     expect(page).to have_text(project.description)
     expect(page).to have_text("$6,000,000.00")
-    expect(page).to have_text(project.pledging_ends_on)
     expect(page).to have_text(project.website)
+  end
+
+  it "shows the number of days remaining if the pledging end date is in the future" do
+    project = Project.create(project_attributes(pledging_ends_on: 1.day.from_now))
+
+    visit project_url(project)
+
+    expect(page).to have_text("1 day remaining")
+  end
+
+  it "shows 'All Done!' if the pledging end date is in the past" do
+    project = Project.create(project_attributes(pledging_ends_on: 1.day.ago))
+
+    visit project_url(project)
+
+    expect(page).to have_text("All Done!")
   end
 end
