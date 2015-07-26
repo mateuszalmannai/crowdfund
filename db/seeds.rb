6cf8b6336059cabde7ba11459dedc7f0a1e4e3d9
mateusz <mateuszalmannai@googlemail.com>
2015-07-26 13:22:23 +0200

    add root route and clean-up formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create!([
+  {
+    name: "Project A",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...",
+    target_pledge_amount: 500000.00,
+    website: "www.projectA.org",
+    pledging_ends_on: "2015-11-02"
+  },
+  {
+    name: "Project B",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...",
+    target_pledge_amount: 10000.00,
+    website: "www.projectB.org",
+    pledging_ends_on: "2016-08-02"
+  },
+  {
+    name: "Project C",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...",
+    target_pledge_amount: 4000.00,
+    website: "www.projectC.org",
+    pledging_ends_on: "2015-04-02"
+  }
+])
